478b825f4bfabcd728d1b73b64ba44488467049d
Jsprofile <jsdeluca01@gmail.com>
2022-10-11 15:31:02 -0300

    Adding ScrollTop button and fix

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const quests = document.querySelectorAll('.quest')
 const answers = document.querySelectorAll('.answer')
 
 quests.forEach(quest => {
-    quest.addEventListener('click', ()=>{
+    quest.addEventListener('click', () => {
         var answer = quest.nextElementSibling
         answer.classList.toggle('activeAnswer')
 
-        answers.forEach((item) =>{
-            if(item != answer){
+        answers.forEach((item) => {
+            if (item != answer) {
                 item.classList.remove('activeAnswer')
             }
         })
@@ -25,7 +25,27 @@ const planos = document.querySelector('.planos')
 
 var planosHeight = planos.clientHeight
 
-if(window.innerWidth <= 800){
+if (window.innerWidth <= 800) {
     planos.style.height = `calc(${planosHeight}px + 100px)`
     console.log(planos.style.height)
 }
+
+
+// ScrollTop
+
+var btnScroll = document.querySelector('.scrollTop')
+
+window.onscroll = () => {
+
+    // ScrollTop appear
+
+    if (window.pageYOffset >= 100) {
+        btnScroll.classList.add('scrollTop-active')
+    } else {
+        btnScroll.classList.remove('scrollTop-active')
+    }
+}
+
+btnScroll.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+})
