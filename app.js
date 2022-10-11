ad8601e65b61e6b3cdb785aa74c02f661efc9907
Jsprofile <jsdeluca01@gmail.com>
2022-10-11 15:17:30 -0300

    Adding text X videos section

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Sessão FAQ
+
 const quests = document.querySelectorAll('.quest')
 const answers = document.querySelectorAll('.answer')
 
@@ -13,3 +15,17 @@ quests.forEach(quest => {
         })
     })
 })
+
+
+
+
+// Ajuste Mobile - Sessão Planos
+
+const planos = document.querySelector('.planos')
+
+var planosHeight = planos.clientHeight
+
+if(window.innerWidth <= 800){
+    planos.style.height = `calc(${planosHeight}px + 100px)`
+    console.log(planos.style.height)
+}
